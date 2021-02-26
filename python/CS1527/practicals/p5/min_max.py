59d97c39b0a2b085abc81435f18ee83f7c5a2ca5
Zsolt Kébel <kebel.zsolt@gmail.com>
2021-02-26 13:08:38 +0000

    improved readability and added unittest

--- python/CS1527/practicals/p5/min_max.py
+++ python/CS1527/practicals/p5/min_max.py
@@ -3,19 +3,38 @@
 
 # Program that finds the minimum and maximum value in a sequence without using a loop
 
-def find_min_max(s):
-    if len(s) > 1:
-        min_val, max_val = find_min_max(s[1:])
-        return min(s[0], min_val), max(s[0], max_val)
-    elif len(s) == 1:
-        return s[0], s[0]  # base case
-    else:
-        return None, None
+import unittest
+from collections import namedtuple
 
 
-s = [2, 1, 4, 5, 10, 0, 3, -3]
+def find_min_max(arr):
+    extreme_values = namedtuple('value', ['min', 'max'])
 
-minimum,  maximum = find_min_max(s)
+    if len(arr) > 1:
+        extremes_of_subarray = find_min_max(arr[1:])
+        return extreme_values(min(arr[0], extremes_of_subarray.min), max(arr[0], extremes_of_subarray.max))
+    else:  # base case
+        try:
+            return extreme_values(arr[0], arr[0])
+        except IndexError:
+            return extreme_values(None, None)
 
-print('min:', minimum)
-print('max:', maximum)
+
+class Tests(unittest.TestCase):
+
+    def test1(self):
+        s = [2, 1, 4, 5, 10, 0, 3, -3]
+        self.assertEqual(find_min_max(s), (-3, 10))
+
+    def test2(self):
+        s = [2, 4, 0, -3]
+        self.assertEqual(find_min_max(s), (-3, 4))
+
+    def test3(self):
+        s = []
+        self.assertEqual(find_min_max(s), (None, None))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
